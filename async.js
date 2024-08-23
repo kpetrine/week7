0be6acac7aff46d4b3135ac10c7b9c04c42a3992
Katie Petrine <kpetrine@gmail.com>
2024-08-23 07:22:20 -0700

    adding files

---
+++ async.js
@@ -0,0 +1,7 @@
+// line 1
+// line 2
+// line 3
+
+console.log("Put food in the oven...");
+//setTimeout(function() {
+//}).
---
+++ scope.js
@@ -0,0 +1,22 @@
+
+(function wrapper(){
+
+})
+if (true) {
+    var age = 48
+    let yearBorn =1975;
+
+}
+
+console.log("My age is:" + age);
+
+//Four score and seven years ago our fathers brought forth, upon this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.
+ let quote = "Four score and seven years ago our fathers brought forth," + 
+            "upon this continent, a new nation, conceived in liberty, and " +
+            "dedicated to the proposition that all men are created equal.";
+
+            console.log(quote);
+
+            let multilineQuote = 'Four score and seven years ago our fathers brought forth, upon this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal."
+';
+console.log(multilineQuote)
